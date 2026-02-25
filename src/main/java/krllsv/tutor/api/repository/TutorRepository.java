f8ddade8e10c033aa0b4f88a4531c8fc25b941da
Kirill <kkirillsolomennikov@gmail.com>
2026-02-25 21:18:54 +0300

    init commit

--- src/main/java/krllsv/tutor/api/repository/TutorRepository.java
+++ src/main/java/krllsv/tutor/api/repository/TutorRepository.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 @Repository
 public class TutorRepository {
 
-    private final ArrayList<Tutor> tutors = new ArrayList<>();
+    private final List<Tutor> tutors = new ArrayList<>();
 
     @PostConstruct
     public void init() {
