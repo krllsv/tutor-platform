865ff918032d175e7ec8f91266bc90f5e029f35b
Kirill <kkirillsolomennikov@gmail.com>
2026-03-18 09:05:37 +0300

    CRUD

---
+++ src/main/java/krllsv/tutor/api/entity/TutorEntity.java
@@ -0,0 +1,54 @@
+package krllsv.tutor.api.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Table(name = "tutors")
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TutorEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "hourly_rate")
+    private BigDecimal hourlyRate;
+    @Column(name = "start_year")
+    private int startYear;
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "subject_id")
+    private SubjectEntity subject;
+
+    @OneToMany(mappedBy = "tutor", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @ToString.Exclude
+    private List<BookingEntity> bookings = new ArrayList<>();
+
+    @OneToMany(mappedBy = "tutor", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @ToString.Exclude
+    private List<ReviewEntity> reviews = new ArrayList<>();
+}
